5229d12ee3d4a26fb4611a4c2cb7596ded381992
Raniya Javed <raniyajaved4@gmail.com>
2025-09-22 07:03:08 +0530

    updated files after some changes

---
+++ FUNCTIONS_PRACTICE.JS/function-declaration&function-expression.js
@@ -0,0 +1,7 @@
+// Function Declaration
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+// You can call before or after its definition
+console.log(greet("Raniya")); // Output: Hello, Raniya!
