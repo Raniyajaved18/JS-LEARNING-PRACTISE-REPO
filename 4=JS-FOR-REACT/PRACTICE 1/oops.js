b9c491beb92ac285cc7a74c525f57e1f4021b4ca
Raniya Javed <raniyajaved4@gmail.com>
2025-09-29 05:55:29 +0530

    ...

--- 4=JS-FOR-REACT/PRACTICE 1/oops.js
+++ 4=JS-FOR-REACT/PRACTICE 1/oops.js
@@ -24,7 +24,7 @@
 
 // 📌 2. Class with Multiple Methods
 
-class Student {
+/* class Student {
     constructor(name, age) {
         this.name = name;
         this.age = age
@@ -41,4 +41,79 @@ class Student {
 
 const s1 = new Student("Raniya", 21);
 s1.introduce();
-s1.study("JS");
+s1.study("JS");  */
+
+// --------------------------------------------------------------------------------
+
+// 📌 3. Inheritance (extends)
+
+/* class Vehicle {
+    constructor(brand) {
+        this.brand = brand;
+
+    }
+    start() {
+         console.log(`${this.brand} engine started 🔑`);
+    }
+}
+// car extends vehicle
+
+class Car extends Vehicle {
+    constructor(brand, color) {
+
+        super(brand); // call parent constructor
+        this.color = color;
+    }
+
+    drive() {
+        console.log(`${this.color} ${this.brand} is driving`);
+
+    }
+}
+
+const myCar = new Car("tesla", "white");
+myCar.start();
+myCar.drive(); */
+
+// --------------------------------------------------------------------------------
+
+/* 📌 4. Objects vs Classes (Key Difference)
+
+Object = actual thing (specific student, car, user).
+
+Class = blueprint (general design).
+
+constructor() = special function that sets up an object.constructor() = special function that sets up an object.
+
+extends + super = inherit properties/methods from parent class.
+
+You can create many objects from one class. */
+
+// --------------------------------------------------------------------------------
+
+// 📌 5. Mini Real-World Example
+
+/* class User {
+  constructor(username, email) {
+    this.username = username;
+    this.email = email;
+  }
+
+  login() {
+    console.log(`${this.username} logged in ✅`);
+  }
+}
+
+// Admin is also a User but with more power
+class Admin extends User {
+  deleteUser(user) {
+    console.log(`${this.username} deleted ${user.username} ❌`);
+  }
+}
+
+const user1 = new User("john", "john@email.com");
+const admin1 = new Admin("adminRaniya", "admin@email.com");
+
+user1.login();    // john logged in ✅
+admin1.login();   // adminRaniya logged in ✅ (inherited from User)
+admin1.deleteUser(user1); // adminRaniya deleted john ❌  */
